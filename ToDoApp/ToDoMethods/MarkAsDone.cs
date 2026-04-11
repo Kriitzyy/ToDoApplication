b07b27d34487100fae93c4cde787e280d60fdb9f
kriitzyy <Akadmn04@gmail.com>
2026-04-12 00:06:15 +0200

    Mark as done completed

--- ToDoApp/ToDoMethods/MarkAsDone.cs
+++ ToDoApp/ToDoMethods/MarkAsDone.cs
@@ -6,7 +6,29 @@ public class MarkAsDone
 {
     public static void MarkTasks(List<ToDoClass> tasks)
     {
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks to mark as done.");
+            return;
+        }
+
+        Console.WriteLine("\nEnter the number of the task to mark as done:");
         
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {tasks[i].Title} {(tasks[i].IsCompleted ? "(Done)" : "")}");
+        }
+
+        if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskNumber >= 1 && taskNumber <= tasks.Count)
+        {
+            tasks[taskNumber - 1].IsCompleted = true;
+            Console.WriteLine("\nTask marked as done successfully!");
+        }
+        else
+        {
+            Console.WriteLine("\nInvalid task number. Please try again.");
+        }
+
     }
 }
 }
--- obj/Debug/net8.0/ToDoConsole.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0b88f4948ebbf5677d0a337bbf2ffb9fd10b7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03ff24d38f1d62fdbca94a1f467d315ed349416d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
